81056d22667aeb8dc59454e80bc4572ef3da54c0
Bárbara Álvarez González <balvarez@fanae126.geol.uniovi.es>
2019-07-31 15:38:15 +0200

    check SFs

--- TTHAnalysis/python/plotter/ttScaleFactors.py
+++ TTHAnalysis/python/plotter/ttScaleFactors.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 import os
-vars = ['muon_eta', 'muon_pt']
+muvars  = ['muon_eta', 'muon_pt']
+elevars = ['ele_eta',  'ele_pt']
 
 #loop over variables
-for variable in vars:
+#muon probe
+for variable in muvars:
     for nbin in range(1,11):
-        print ">>>> Variable: ",variable, " and bin number = ",nbin
+        #print ">>>> Variable: ",variable, " and bin number = ",nbin
         #cmd = "root -l -b -q \'ttScaleFactors.C+(\"2017\", \"eTmuL\", \"eTmuT\", \""+variable+"\", "+str(nbin)+")\'"
-        cmd = "root -l -b -q \'ttScaleFactors.C+(\"2017\", \"eTmuL\", \"eTmuT\", \""+variable+"\", "+str(nbin)+")\'"
-        print cmd
-        os.system(cmd)
+        cmdmu  = "root -l -b -q \'ttScaleFactors.C+(\"2017\", \"eTmuL\", \"eTmuT\", \""+variable+"\", "+str(nbin)+")\'"
+        #print cmd
+        os.system(cmdmu)
+#electron probe
+for variable in elevars:
+    for nbin in range(1,11):
+        #print ">>>> Variable: ",variable, " and bin number = ",nbin
+        #cmd = "root -l -b -q \'ttScaleFactors.C+(\"2017\", \"eTmuL\", \"eTmuT\", \""+variable+"\", "+str(nbin)+")\'"
+        cmdele = "root -l -b -q \'ttScaleFactors.C+(\"2017\", \"muTeL\", \"muTeT\", \""+variable+"\", "+str(nbin)+")\'"
+        #print cmd
+        os.system(cmdele)
